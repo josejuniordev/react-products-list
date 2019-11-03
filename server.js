5dc6d9f62783771df4f8b971d3b1671dba2957d3
José <josejuniordev@gmail.com>
2019-11-03 04:10:55 -0300

    feat: fix server.js

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'LayoutContaineriner.js.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
